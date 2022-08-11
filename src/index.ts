cbcfc55f06dc55a09d2a13c8254df2ae984c0ab6
simoncho <simonmcho@gmail.com>
2022-08-11 15:58:24 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { registerComponent } from 'ub-shared';
 
 import { Component } from './hello-world';
-
+console.log('submodules test');
 /**
  * Feel free to register whatever you need to export.
  * IMPORTANT: Registration ordering is important.
